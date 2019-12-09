7b897f48d43086f3c7e6ab08c78571e5147fbc1d
mohamedsaid000 <58403627+mohamedsaid000@users.noreply.github.com>
2019-12-08 21:04:11 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "id11857291_test000";
-$password = "password55";
-$db = "id11857291_test";
+$username = "";
+$password = "";
+$db = "";
 
 // Create connection
 $link = new mysqli($servername, $username, $password, $db);
